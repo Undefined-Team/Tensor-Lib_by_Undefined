a38403de78fbda9a867bd5f7c03673b688cec772
sbedene <sylvainbedene@gmail.com>
2019-11-01 21:34:32 +0100

    add tens type fp print

--- res/src/type_get.c
+++ res/src/type_get.c
@@ -3,7 +3,10 @@
 ud_arr_type    *ud_tens_type_tens(void)
 {
     static ud_arr_type *type_tensor = NULL;
-    if (!type_tensor) type_tensor = ud_arr_type_get(ud_tensor*);
-    type_tensor->fp_print = &ud_tens_print;
+    if (!type_tensor)
+    {
+        type_tensor = ud_arr_type_get(ud_tensor*);
+        type_tensor->fp_print = &ud_tens_print;
+    }
     return type_tensor;
 }
